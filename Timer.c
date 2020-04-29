c67543a26c4c57c79d76b23730ebb9c3a8416f4c
choeksema <56086870+choeksema@users.noreply.github.com>
2020-04-29 14:37:12 -0400

    Updated

--- Timer.c
+++ Timer.c
@@ -1,7 +1,17 @@
+ /*****************************************************************************
+ * Timer.c
+ * Contains functions to initialize a timer port
+ * Caleb Hoeksema, Gregory Huras
+ * April 2020
+ ****************************************************************************/
+
+
 #include "Timer.h"
 #include "utils.h"
 
-void Timer_Init(void) { //Close, could have issues here
+
+// Initilize timer 1 to run the beeper on PE8
+void Timer_Init(void) {
 	
 	TIMER_CLOCK_ENABLE();
 	
@@ -12,8 +22,7 @@ void Timer_Init(void) { //Close, could have issues here
 	TIM1->PSC = 79;					//80MHz clock --> clock/(PSC+1) = 1MHz, matches useconds
 	
 	// Auto-reload (also 16b)
-	TIM1->ARR = 5000;		//1MHz clock (see above), PWM period = 5ms = 200hz--> ARR = clock*PWM - 1
-	// Depending on the frequency of the PWM will create teh different tones
+	TIM1->ARR = 0;		// Varies based on desired output
 	
 	// Set PWM mode
 	TIM1_MODE(MODE_PWM);
@@ -30,14 +39,16 @@ void Timer_Init(void) { //Close, could have issues here
 	// Main O/P enable: 0 = off, 1 = en
 	OP_ENABLE(OP_ON);
 	
-	// Dont know if thisis right - Sets the duty cycle - half duty cycle
-	TIM1->CCR1 = (TIM1->ARR)/2;
+	// 50% duty cycle
+	TIM1->CCR1 = (TIM1->ARR)/2;		// Should always be 50%
 	
 	// Enable timer 1
 	SET_BITS(TIM1->CR1, TIM_CR1_CEN);
 	
 }
 
+
+// Enable timer 1's clock
 void TIMER_CLOCK_ENABLE(void) {
 	
 	// Set timer 1 clock
