2bcbf655b87f8ec495dbaf8ec785fb23b393b39b
choeksema <56086870+choeksema@users.noreply.github.com>
2020-05-02 16:07:09 -0400

    Fixed macros, has bad debouncing but works

--- Keypad.c
+++ Keypad.c
@@ -20,65 +20,177 @@
 uint16_t getKey(void) {
 	
 	// Scan row 1
-	ROW_BUS(0111);
+	ROW1_MODE(0);
+	ROW2_MODE(1);
+	ROW3_MODE(1);
+	ROW4_MODE(1);
 	Delay_ms(15);
 	
 	// Check columns
-	if ((COL_PORT & COL1_BIT) == 0)
-		return(0x1);
-	if ((COL_PORT & COL2_BIT) == 0)
-		return(0x2);
-	if ((COL_PORT & COL3_BIT) == 0)
-		return(0x3);
-	if ((COL_PORT & COL4_BIT) == 0)
-		return(0xA);
+	if ((COL_PORT & COL1_BIT) == 0) {
+		if (0x1 == flag)
+			return(0x1);
+		else {
+			flag = 0x1;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL2_BIT) == 0) {
+		if (0x2 == flag)
+			return(0x2);
+		else {
+			flag = 0x2;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL3_BIT) == 0) {
+		if (0x3 == flag)
+			return(0x3);
+		else {
+			flag = 0x3;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL4_BIT) == 0) {
+		if (0xA == flag)
+			return(0xA);
+		else {
+			flag = 0xA;
+			return(0x10);
+		}
+	}
 	
 	// Scan row 2
-	ROW_BUS(1011);
+	ROW1_MODE(1);
+	ROW2_MODE(0);
+	ROW3_MODE(1);
+	ROW4_MODE(1);
 	Delay_ms(15);
 
 	// Check columns	
-	if ((COL_PORT & COL1_BIT) == 0)
-		return(0x4);
-	if ((COL_PORT & COL2_BIT) == 0)
-		return(0x5);
-	if ((COL_PORT & COL3_BIT) == 0)
-		return(0x6);
-	if ((COL_PORT & COL4_BIT) == 0)
-		return(0xB);
+	if ((COL_PORT & COL1_BIT) == 0) {
+		if (0x4 == flag)
+			return(0x4);
+		else {
+			flag = 0x4;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL2_BIT) == 0) {
+		if (0x5 == flag)
+			return(0x5);
+		else {
+			flag = 0x5;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL3_BIT) == 0) {
+		if (0x6 == flag)
+			return(0x6);
+		else {
+			flag = 0x6;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL4_BIT) == 0) {
+		if (0xB == flag)
+			return(0xB);
+		else {
+			flag = 0xB;
+			return(0x10);
+		}
+	}
 	
 	// Scan row 3
-	ROW_BUS(1101);
+	ROW1_MODE(1);
+	ROW2_MODE(1);
+	ROW3_MODE(0);
+	ROW4_MODE(1);
 	Delay_ms(15);
 	
 	// Check columns	
-	if ((COL_PORT & COL1_BIT) == 0)
-		return(0x7);
-	if ((COL_PORT & COL2_BIT) == 0)
-		return(0x8);
-	if ((COL_PORT & COL3_BIT) == 0)
-		return(0x9);
-	if ((COL_PORT & COL4_BIT) == 0)
-		return(0xC);
+	if ((COL_PORT & COL1_BIT) == 0) {
+		if (0x7 == flag)
+			return(0x7);
+		else {
+			flag = 0x7;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL2_BIT) == 0) {
+		if (0x8 == flag)
+			return(0x8);
+		else {
+			flag = 0x8;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL3_BIT) == 0) {
+		if (0x9 == flag)
+			return(0x9);
+		else {
+			flag = 0x9;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL4_BIT) == 0) {
+		if (0xC == flag)
+			return(0xC);
+		else {
+			flag = 0xC;
+			return(0x10);
+		}
+	}
 	
 	// Scan row 4
-	ROW_BUS(1110);
+	ROW1_MODE(1);
+	ROW2_MODE(1);
+	ROW3_MODE(1);
+	ROW4_MODE(0);
 	Delay_ms(15);
 	
 	// Check columns	
-	if ((COL_PORT & COL1_BIT) == 0)
-		return(0xE);
-	if ((COL_PORT & COL2_BIT) == 0)
-		return(0x0);
-	if ((COL_PORT & COL3_BIT) == 0)
-		return(0xF);
-	if ((COL_PORT & COL4_BIT) == 0)
-		return(0xD);
+	if ((COL_PORT & COL1_BIT) == 0) {
+		if (0xE == flag)			// 0xE is *
+			return(0xE);
+		else {
+			flag = 0xE;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL2_BIT) == 0) {
+		if (0x0 == flag)
+			return(0x0);
+		else {
+			flag = 0x0;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL3_BIT) == 0) {
+		if (0xF == flag)			// 0xF is #
+			return(0xF);
+		else {
+			flag = 0xF;
+			return(0x10);
+		}
+	}
+	if ((COL_PORT & COL4_BIT) == 0) {
+		if (0xD == flag)
+			return(0xD);
+		else {
+			flag = 0xD;
+			return(0x10);
+		}
+	}
 	
 	// Set all rows high
-	ROW_BUS(1111);
+	ROW1_MODE(1);
+	ROW2_MODE(1);
+	ROW3_MODE(1);
+	ROW4_MODE(1);
 	
 	// Return an error
+	flag = 0x10;
 	return(0x10);
 	
 }
